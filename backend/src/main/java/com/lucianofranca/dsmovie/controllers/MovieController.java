c404429e82ad0d9822aaa76fbf8efe848918a1d2
Luciano de franca <lucianodefranca86@gmail.com>
2022-01-13 22:02:42 -0300

    endpoint busca movie por ID

--- backend/src/main/java/com/lucianofranca/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/lucianofranca/dsmovie/controllers/MovieController.java
@@ -1,9 +1,12 @@
 package com.lucianofranca.dsmovie.controllers;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,11 @@ public class MovieController {
 	@GetMapping
 	public Page<MovieDTO> findAll(Pageable pageable) {
 		return service.findAll(pageable);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public MovieDTO findById(@PathVariable Long id) {
+		return service.findById(id);
 	}
 
 }
--- backend/src/main/java/com/lucianofranca/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/lucianofranca/dsmovie/services/MovieService.java
@@ -24,5 +24,12 @@ public class MovieService {
 		Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
 		return page;
 	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Movie result = repository.findById(id).get();
+		MovieDTO dto = new MovieDTO(result);
+		return dto;
+	}
 
 }
