73c08672453908b40883d79fd1c8c18c625fd9c5
Luciano de franca <lucianodefranca86@gmail.com>
2022-01-14 23:15:10 -0300

    UseState, UseEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,16 +1,25 @@
-import { logDOM } from "@testing-library/react";
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import { log } from "console";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/request";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data);
-        });
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() =>{
+
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(response.data);
+                setPageNumber(data.number);
+            });
+
+    }, []);
+
 
     return (
         <>
